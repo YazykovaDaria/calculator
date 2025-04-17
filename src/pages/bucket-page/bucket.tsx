7d5c629cf8c108cd6467b21a1897be02b45cfc66
YazykovaDaria <yazykovadasva@gmail.com>
2025-04-18 00:50:11 +0300

    feat: bucket page

--- src/pages/bucket-page/bucket.tsx
+++ src/pages/bucket-page/bucket.tsx
@@ -1,3 +1,60 @@
+import { useAppSelector } from "@entities";
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeadCell,
+    TableRow,
+} from "flowbite-react";
+
 export const BucketPage = () => {
-    return <section>bucket page</section>;
+    const items = useAppSelector((state) => state.bucket);
+    if (items.length === 0)
+        return (
+            <p className="font-semibold text-xl text-center">
+                Нет товаров в корзине
+            </p>
+        );
+
+    const totalPrice = items.reduce((acc, item) => {
+        acc += item.cost;
+        return acc;
+    }, 0);
+
+    return (
+        <section className="flex flex-col gap-4">
+            <h2 className="font-semibold text-xl">Корзина</h2>
+            <div className="overflow-x-auto">
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableHeadCell>Наименование</TableHeadCell>
+                            <TableHeadCell>ед.</TableHeadCell>
+                            <TableHeadCell>кол-во</TableHeadCell>
+                            <TableHeadCell>сумма</TableHeadCell>
+                        </TableRow>
+                    </TableHead>
+                    <TableBody className="divide-y">
+                        {items.map((item, i) => (
+                            <TableRow className="bg-white" key={i}>
+                                <TableCell className="whitespace-nowrap font-medium text-gray-950">
+                                    {item.name}
+                                </TableCell>
+                                <TableCell>{item.unit}</TableCell>
+
+                                <TableCell>{item.quantity}</TableCell>
+                                <TableCell>{item.cost}</TableCell>
+                            </TableRow>
+                        ))}
+                        <TableRow className="whitespace-nowrap font-medium text-gray-950">
+                            <TableCell colSpan={3}>Итого:</TableCell>
+
+                            <TableCell>{totalPrice}</TableCell>
+                        </TableRow>
+                    </TableBody>
+                </Table>
+            </div>
+        </section>
+    );
 };
