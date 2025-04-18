15f76dec1045bb360424a87db3a82468a30b01fb
YazykovaDaria <yazykovadasva@gmail.com>
2025-04-18 13:50:43 +0300

    feat: pipe calculate

--- src/shared/helpers/calculates.ts
+++ src/shared/helpers/calculates.ts
@@ -2,12 +2,17 @@ export const calculateArea = (width: number, height: number) => {
     return width * height;
 };
 
+const roundToTwo = (num: number) => {
+    return Math.round(num * 100) / 100;
+};
+
 type CalculateListArgs = {
     listWidth: number;
     listLenght?: number;
     area: number;
     listCost: number;
 };
+
 export const calculateList = ({
     listWidth,
     listLenght = 1,
@@ -15,10 +20,10 @@ export const calculateList = ({
     listCost,
 }: CalculateListArgs) => {
     const listArea = calculateArea(listWidth, listLenght);
-    const listsNeeded = Math.ceil(area / listArea);
-    const cost = listsNeeded * listCost;
+    const totalLists = Math.ceil(area / listArea);
+    const totalListPrice = roundToTwo(totalLists * listCost);
 
-    return { cost, listsNeeded };
+    return { totalListPrice, totalLists };
 };
 
 type ScrewsArgs = {
@@ -28,8 +33,34 @@ type ScrewsArgs = {
 };
 
 export const calculateScrews = ({ area, cost, val }: ScrewsArgs) => {
-    const screwsNeded = Math.ceil(area * val);
-    const price = cost * screwsNeded;
+    const totalScrews = Math.ceil(area * val);
+    const totalScrewsPrice = roundToTwo(cost * totalScrews);
 
-    return { price, screwsNeded };
+    return { totalScrews, totalScrewsPrice };
+};
+
+type PipeArgs = {
+    pipeWidth: number;
+    step: number;
+    width: number;
+    length: number;
+    price: number;
+};
+
+export const calculatePipes = ({
+    pipeWidth,
+    step,
+    width,
+    length,
+    price,
+}: PipeArgs) => {
+    const pipeWidthM = pipeWidth / 1000;
+    const totalStep = step + pipeWidthM;
+    const horizontalLength = (width - pipeWidthM) / totalStep + 1;
+    const verticalLength = (length - pipeWidthM) / totalStep + 1;
+
+    const totalPipeLength = horizontalLength * length + verticalLength * width;
+
+    const totalPipePrice = roundToTwo(totalPipeLength * price);
+    return { totalPipeLength: Math.ceil(totalPipeLength), totalPipePrice };
 };
